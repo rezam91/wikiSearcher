c0ae93acbffe4fd28e50832ae2a41efac04b7564
Reza <r.mehralizadeh@gmail.com>
2025-07-11 18:29:56 +0330

    first initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  useEffect(() => {
+    document.title = 'Wiki Searcher'
+  },[])
+  return (
+    <>
+      <div>hi</div>
+    </>
+  )
+}
+
+export default App
