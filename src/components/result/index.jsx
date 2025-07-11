dd4c331407549e607e05ec945636bae78362ce3a
Reza <r.mehralizadeh@gmail.com>
2025-07-11 20:37:08 +0330

    feat(result): result is being generated by table

--- src/components/result/index.jsx
+++ src/components/result/index.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
+
 const Result = ( {searchedKeyword} ) => {
     return (
         <>
             {!!searchedKeyword && (
                 <div>
                     <div>result:</div>
-                    <ul>
-                        {searchedKeyword.query.search.map((item) => (
-                            <li>{item.title}</li>
-                        ))}
-                    </ul>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Row</th>
+                                <th>Result</th>
+                                <th>word Count</th>
+                                <th>Time to Read (min)</th>
+                                <th>Page</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {searchedKeyword.query.search.map((item, index) => (
+                                <tr key={item.pageid}>
+                                    <td>{index+1}</td>
+                                    <td>{item.title}</td>
+                                    <td>{item.wordcount}</td>
+                                    <td>{item.wordcount/200}</td>
+                                    <td><a href={`https://en.wikipedia.org/?curid=${item.pageid}`}>Visit Page!</a></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                     <div>end</div>
                 </div>
             )}
